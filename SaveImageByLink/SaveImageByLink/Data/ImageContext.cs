0cb2e6679abcc9e817224c2ea9815d595021ce3b
Mykyta Tishkin <87891550+mykytatishkin@users.noreply.github.com>
2023-08-25 00:25:57 +0200

    init

---
+++ SaveImageByLink/SaveImageByLink/Data/ImageContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SaveImageByLink.Models;
+
+namespace SaveImageByLink.Data
+{
+	public class ImageContext : DbContext
+	{
+        public ImageContext(DbContextOptions<ImageContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<SaveImageByLink.Models.Image> Images { get; set; } = default!;
+    }
+}
---
+++ SaveImageByLink/SaveImageByLink/Models/Image.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace SaveImageByLink.Models
+{
+	public class Image
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		[MaxLength]		
+		public string Link { get; set; }
+	}
+}
+
